e92e9b14ad3d665c63da0690dcbdb0dddddcac23
Aslan0N <thenzrv@gmail.com>
2023-06-03 12:40:10 +0400

    login

--- src/Pages/NewLogin.jsx
+++ src/Pages/NewLogin.jsx
@@ -8,6 +8,37 @@ const NewLogin = () => {
         setPage((value)=> !value)
     }
 
+    const [login, setLogin] = useState({
+        username:'',
+        password:'',
+    })
+    const source = {
+        username:'Aslan Nazar',
+        password:'12345',
+    }
+
+    const handleChange = (e)=>{
+        setLogin({...login, [e.target.name]: e.target.value})
+    }
+    const [success, setSuccess] = useState(false)
+    const submitForm = (e) =>{
+        e.preventDefault()
+        if(source.name == login.name && source.password == login.password){
+            console.log(login)  
+            setSuccess(true)
+        }else{
+            changePage()
+        }
+        setLogin({})
+
+    }
+    const [eye, setEye] = useState(false)
+    const changeLock = ()=>{
+        setEye((lock)=> !lock)
+    }
+    
+
+
   return (
     <>
         <section id='new'>
@@ -15,17 +46,26 @@ const NewLogin = () => {
         <div className='fa-solid fa-pen' onClick={changePage} style={{backgroundColor:`${page ? "white" : "red"}`, color:`${page ? "red" : "white"}`}}></div>
             <div className="login-top">
                 <h3>LOGIN</h3>
-                <form>
+                {
+                    success ?
+                    <div className="suc-card">
+                    <i className="fa-solid fa-check"></i>
+                    <h5>Welcome <span>{source.username}</span></h5>
+                </div> :
+                <form onSubmit={submitForm}>
                     <label htmlFor="">
-                        <input type="text" required />
+                        <input type="text" name='username' onChange={handleChange} required />
                         <span>Username</span>
                     </label>
                     <label htmlFor="">
-                        <input type="password" required />
+                        <input type={eye?"text":"password"} name='password' onChange={handleChange} required />
                         <span>Password</span>
+                        <i className={eye ? "fa-solid fa-eye" : "fa-solid fa-eye-slash"} onClick={changeLock}></i>
                     </label>
-                </form>
                     <button>GO</button>
+                </form>
+                }
+                
                 <p>Forgot your password?</p>
             </div>
         </div>
